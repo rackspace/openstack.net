446e43f09de003191f4fe1bbaefc36df4096b033
Jon Walton <jon.walton@rackspace.co.uk>
2013-07-30 11:17:04 +0800

    fixed ExecuteRESTRequest and StreamRESTRequest not refreshing auth token if already exists

--- src/corelib/Providers/Rackspace/ProviderBase.cs
+++ src/corelib/Providers/Rackspace/ProviderBase.cs
@@ -78,10 +78,10 @@ namespace net.openstack.Providers.Rackspace
             if (headers == null)
                 headers = new Dictionary<string, string>();
 
-            if (!headers.ContainsKey("X-Auth-Token"))
-            {
-                headers.Add("X-Auth-Token", IdentityProvider.GetToken(identity, isRetry));
-            }
+            if (headers.ContainsKey("X-Auth-Token"))
+                headers.Remove("X-Auth-Token");
+
+            headers.Add("X-Auth-Token", IdentityProvider.GetToken(identity, isRetry));
 
             string bodyStr = null;
             if (body != null)
@@ -125,10 +125,10 @@ namespace net.openstack.Providers.Rackspace
             if (headers == null)
                 headers = new Dictionary<string, string>();
 
-            if (!headers.ContainsKey("X-Auth-Token"))
-            {
-                headers.Add("X-Auth-Token", IdentityProvider.GetToken(identity, isRetry));
-            }
+            if (headers.ContainsKey("X-Auth-Token"))
+                headers.Remove("X-Auth-Token");
+
+            headers.Add("X-Auth-Token", IdentityProvider.GetToken(identity, isRetry));
 
             if (string.IsNullOrWhiteSpace(requestSettings.UserAgent))
                 requestSettings.UserAgent = GetUserAgentHeaderValue();
