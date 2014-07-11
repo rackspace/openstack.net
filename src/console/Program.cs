a977801317f34137dd4ab1c848d79c7fb3518a15
Alan Quillin <alan.quillin@rackspace.com>
2014-07-11 09:50:14 -0500

    Fixed the cloudserver details not properly returning the ip addresses

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using net.openstack.Providers.Rackspace;
 using Net.OpenStack.Testing.Integration;
 
 namespace net.openstack.console
@@ -8,6 +8,10 @@ namespace net.openstack.console
         static void Main(string[] args)
         {
             Bootstrapper.Initialize();
+
+            var identityProvider = new IdentityProvider(usInstanceUrlBase: "https://identity-internal.api.rackspacecloud.com", 
+                ukInstanceUrlBase: "https://lon.identity-internal.api.rackspacecloud.com");
+
         }
     }
 }
--- src/corelib/Core/Domain/ServerAddresses.cs
+++ src/corelib/Core/Domain/ServerAddresses.cs
@@ -1,8 +1,66 @@
+//namespace net.openstack.Core.Domain
+//{
+//    public class ServerAddresses
+//    {
+//        public Address[] Private { get; set; }
+//        public Address[] Public { get; set; }
+//    }
+//}
+
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+
 namespace net.openstack.Core.Domain
 {
-    public class ServerAddresses
+    /// <summary>
+    /// Represents a map of network labels to collections of IP addresses.
+    /// </summary>
+    /// <remarks>
+    /// The keys of this collection are network labels (see <see cref="CloudNetwork.Label"/>),
+    /// and the values are collections of IP addresses.
+    /// </remarks>
+    /// <threadsafety static="true" instance="false"/>
+    [DataContract]
+    public class ServerAddresses : Dictionary<string, IEnumerable<Address>>
     {
-        public Address[] Private { get; set; }
-        public Address[] Public { get; set; }
+        /// <summary>
+        /// Gets the server IP addresses associated with the <c>private</c> network.
+        /// </summary>
+        [IgnoreDataMember]
+        public IEnumerable<Address> Private
+        {
+            get
+            {
+                if (!this.Any(x => x.Key == "private"))
+                    return null;
+
+                return this.First(x => x.Key == "private").Value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the server IP addresses associated with the <c>public</c> network.
+        /// </summary>
+        [IgnoreDataMember]
+        public IEnumerable<Address> Public
+        {
+            get
+            {
+                if (!this.Any(x => x.Key == "public"))
+                    return null;
+
+                return this.First(x => x.Key == "public").Value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the server IP addresses associated with the <c>public</c> network.
+        /// </summary>
+        [IgnoreDataMember]
+        public Dictionary<string, IEnumerable<Address>> Other
+        {
+            get { return this.Where(x => x.Key != "public" && x.Key != "private").ToDictionary(x => x.Key, x => x.Value);}
+        }
     }
 }
